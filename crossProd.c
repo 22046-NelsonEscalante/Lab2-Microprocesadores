fffe81378a4c82c501d5c3c996c3366c6e45cce6
22046-NelsonEscalante <esc22046@uvg.edu.gt>
2023-08-08 13:29:54 -0600

    Established the parallel region. Added a header.

--- crossProd.c
+++ crossProd.c
@@ -1,3 +1,19 @@
+/*
+--------------------------------------------
+crossProd.c
+--------------------------------------------
+Universidad del Valle de Guatemala
+CC3086 - Programacion de Microprocesadores
+Laboratorio 2
+
+Nelson Escalante - 22046
+Nicolle Gordillo - 22246
+--------------------------------------------
+Calcular el producto cruz entre dos vectores
+de tamaño N, establecido por el usuario.
+--------------------------------------------
+*/
+
 #include <stdio.h>
 #include <omp.h>
 
@@ -9,10 +25,25 @@ int main () {
     
     double A[N], B[N], C[N], X;
 
-
     printf("\nIngrese el numero de threads con los que desea trabajar.\n");
     scanf("%d", X);
     omp_set_num_threads(X);
+
+    int i, j;
+	
+	for (i=0; i<N; i++) A[i] = i;
+	for (i=0; i<N; i++) B[i] = i;
+
+
+    for (i = 0; i < N; i++) {
+        C[i] = A[i];
+    }
+
+    #pragma omp parallel
+    {
+        #pragma omp for reduction(+:sum)
+        //Código a ejecutar
+    }
 
     return 0;
 }
